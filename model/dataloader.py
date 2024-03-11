9730806b9fa1e502d7400edf9db7472b561eaec6
jun048098 <jun048098@gmail.com>
2024-03-11 12:25:49 +0900

    dataloader.py len

--- model/dataloader.py
+++ model/dataloader.py
@@ -1,3 +1,16 @@
 import pandas as pd
 from tqdm.auto import tqdm
 import torch
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(
+            self,
+            data_file
+    ):
+        self.data = pd.read_csv(data_file)
+
+    def __getitem__(self, idx):
+        return
+    
+    def __len__(self):
+        return len(self.data)
