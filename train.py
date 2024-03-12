162fa7ad95a1166ae6919623da1213864fcfa1c0
jun048098 <jun048098@gmail.com>
2024-03-13 02:04:52 +0900

    rename yaml folder

--- train.py
+++ train.py
@@ -1,1 +1,9 @@
 import os
+
+import torch
+from dataloader import CustomDataset
+from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
+from utils import compute_pearson_correlation, load_yaml, seed_everything
+
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+prj_dir  = os.path.dirname(os.path.abspath(__file__))
