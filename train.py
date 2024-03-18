3ceb9afc1a7c7e8588b5ff1c33dfded448b6c4fb
jun048098 <jun048098@gmail.com>
2024-03-18 21:14:04 +0900

    'train.py'

--- train.py
+++ train.py
@@ -3,8 +3,11 @@ import os
 import torch
 from torch.utils.data import random_split
 from dataloader import CustomDataset
+
 from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
-from utils import load_yaml, seed_everything, get_argumnets
+
+from utils import load_yaml, seed_everything, get_argumnets, start_wandb
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 prj_dir  = os.path.dirname(os.path.abspath(__file__))
@@ -16,13 +19,16 @@ if __name__ == "__main__":
     config = load_yaml(path=config_path)
     seed_everything(config['seed'])
 
+    start_wandb(run_name=config['name'])
+
     print("cuda available:", torch.cuda.is_available())
+
     model = AutoModelForSequenceClassification.from_pretrained(
         config['architecture'],
         num_labels=1,
         ignore_mismatched_sizes=True
         )
-
+   
     train_text_dataset = CustomDataset(
         data_file=config['data_folder']['train_data'],
         state='train',
@@ -33,12 +39,14 @@ if __name__ == "__main__":
     )
 
     dataset_size = len(train_text_dataset)
-    train_size = int(dataset_size * 0.9)
+    train_size = int(dataset_size * 0.99)
     validation_size = dataset_size - train_size
 
     train_dataset, validation_dataset = random_split(train_text_dataset, [train_size, validation_size])
     print(f'train data: {len(train_dataset), }, val data: {len(validation_dataset)}')
 
+    del[[train_text_dataset]]
+    
     args = TrainingArguments(
         output_dir=os.path.join(prj_dir, "save_folder", config["name"]),
         evaluation_strategy="epoch",
@@ -50,12 +58,12 @@ if __name__ == "__main__":
         weight_decay=config["weight_decay"],
         load_best_model_at_end=True,
         dataloader_num_workers=4,
-        logging_steps=100,
+        logging_steps=2000,
         seed=config["seed"],
         group_by_length=True,
         lr_scheduler_type=config["scheduler"],
     )
-    
+
 
     trainer = Trainer(
         model=model,
@@ -63,6 +71,7 @@ if __name__ == "__main__":
         train_dataset=train_dataset,
         eval_dataset=validation_dataset,
     )
+
 
     trainer.train()
     
