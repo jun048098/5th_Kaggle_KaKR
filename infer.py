4c9a7cc1a9229afe4cbad17a427745bff809d4cb
jun048098 <jun048098@gmail.com>
2024-03-18 21:14:42 +0900

    'infer.py'

---
+++ infer.py
@@ -0,0 +1,51 @@
+import os
+
+import pandas as pd
+import torch
+import transformers
+from dataloader import CustomDataset
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+from utils import load_yaml
+
+prj_dir = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == "__main__":
+    config_path = os.path.join(prj_dir, "config_yaml", "test.yaml")
+    config = load_yaml(config_path)
+
+    model_list = ['base']
+    model_name = model_list[0]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(
+        os.path.join(prj_dir, "save_folder", config["checkpoint"][model_name])
+    )
+
+    test_text_dataset = CustomDataset(
+        data_file=config["data_folder"]["test_data"],
+        state="test",
+        text_column='comment_text',
+        target_column=None,
+        max_length=256,
+        model_name=config["name"][model_name],
+    )
+    test_dataloader = DataLoader(
+        dataset=test_text_dataset,
+        batch_size=4,
+        num_workers=0,
+        shuffle=False,
+        drop_last=False,
+    )
+    score = []
+    model.to(device)
+    model.eval()
+    with torch.no_grad():
+        for batch_id, x in enumerate(tqdm(test_dataloader)):
+            y_pred = model(x["input_ids"].to(device))
+            logits = y_pred.logits
+            y_pred = logits.detach().cpu().numpy()
+            score.extend(y_pred)
+    score = list(float(i) for i in score)
+    output = pd.read_csv(config["data_folder"]["submissions"])
+    output["prediction"] = score
+    output.to_csv(f"./output/{model_name}.csv", index=False)
